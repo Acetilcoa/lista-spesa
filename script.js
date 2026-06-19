98dfcfcbffc3771e0387717c002542489f309bc3
Daniele Colini <daniele.colini@libero.it>
2026-06-19 19:42:17 +0200

    Primo commit: progetto lista della spesa

---
+++ script.js
@@ -0,0 +1,24 @@
+const bottone = document.getElementById("aggiungiBtn");
+const input = document.getElementById("nuovoElemento");
+const lista = document.getElementById("lista");
+const messaggioErrore= document.getElementById("messaggioErrore");
+let contatore = 0;
+
+bottone.addEventListener("click", function() {
+  const testo = input.value;
+  if (testo=== "") {
+    messaggioErrore.textContent = "Inserire un valore valido";
+    return;
+  }
+  messaggioErrore.textContent = "";
+  contatore = contatore + 1;
+  const nuovoLi = document.createElement("li");
+  nuovoLi.textContent = `${contatore}. ${testo}`;
+  lista.appendChild(nuovoLi);
+});
+
+function svuotaLista() {
+  contatore = 0;
+  lista.innerHTML = "";
+  input.value = "";
+}
